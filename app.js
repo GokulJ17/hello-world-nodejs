19f55413b501be712c25151a7eddc2e28c51bb00
Gokul J <gokulj1710@gmail.com>
2024-06-14 14:45:48 +0530

    app.js is updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello From GokulJ!');
+  res.send('Hello From Gokulvidhiya!');
 });
 
 app.listen(3000, () => {
